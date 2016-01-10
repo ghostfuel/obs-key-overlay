9f89064b7950842e31458fc233a1b5a6452ffe39
Mitchell Bellamy <13374080@students.lincoln.ac.uk>
2016-01-10 18:27:08 +0000

    Re-sync

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,14 +1,16 @@
 #include "main.h"
-//#include "obs.h"
 #include "obs-module.h"
-#include "obs-source.h"
 
-//OBS_DECLARE_MODULE()
+OBS_DECLARE_MODULE()
+//OBS_MODULE_USE_DEFAULT_LOCALE("obs-key-overlay", "en-US")
 
-extern struct obs_source_info my_source;
+//extern struct obs_source_info create_overlay_info_source;
 
 bool obs_module_load(void)
 {
+	//create_overlay_info_source = { 0 };
+	//create_overlay_info_source.id = "overlay_source";
+	//obs_register_source(&create_overlay_info_source);
 	//obs_register_source(&my_source);
 	return true;
 }
@@ -16,13 +18,4 @@ bool obs_module_load(void)
 void obs_module_unload(void)
 {
 
-}
-
-main::main()
-{
-}
-
-
-main::~main()
-{
 }
